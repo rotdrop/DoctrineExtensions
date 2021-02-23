8fddcd53d3280cebe02e5421303e476e8d197e20
Claus-Justus Heine <himself@claus-justus-heine.de>
2021-02-23 10:02:47 +0100

    Use microtime resolution also for DateTimeImmutable, there is no reason to have this different from the DateTime case.

--- src/Timestampable/Mapping/Event/Adapter/ORM.php
+++ src/Timestampable/Mapping/Event/Adapter/ORM.php
@@ -49,7 +49,8 @@ final class ORM extends BaseAdapterORM implements TimestampableAdapter
         }
 
         if (isset($mapping['type']) && in_array($mapping['type'], ['date_immutable', 'time_immutable', 'datetime_immutable', 'datetimetz_immutable'], true)) {
-            return new \DateTimeImmutable();
+            return \DateTimeImmutable::createFromFormat('U.u', number_format(microtime(true), 6, '.', ''))
+                ->setTimeZone(new \DateTimeZone(date_default_timezone_get()));
         }
 
         return \DateTime::createFromFormat('U.u', number_format(microtime(true), 6, '.', ''))
