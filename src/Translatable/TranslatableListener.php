de0563c0a0b997a8d4f6789b97f66ed7174fff32
Claus-Justus Heine <himself@claus-justus-heine.de>
2021-05-19 00:04:45 +0200

    WIP Translatable: back-translate new translation if value in default translation is null.

--- src/Translatable/TranslatableListener.php
+++ src/Translatable/TranslatableListener.php
@@ -135,6 +135,13 @@ class TranslatableListener extends MappedEventSubscriber
     private ?string $defaultTranslationValue = null;
 
     /**
+     * Tracks empty fields in default locale
+     *
+     * @var array
+     */
+    private $missingInDefaultLocale = [];
+
+    /**
      * Specifies the list of events to listen
      *
      * @return string[]
@@ -549,10 +556,16 @@ class TranslatableListener extends MappedEventSubscriber
                                ||
                                (isset($config['fallback'][$field]) && $config['fallback'][$field]));
 
-                $originalValue = null;
-                if ($translated === $this->defaultTranslationValue && $doFallback) {
+                if ($doFallback) {
                     $originalValue = $meta->getFieldValue($object, $field);
-                    $translated = $this->getFallbackTranslation($originalValue);
+                    if (empty($originalValue)) {
+                        $this->missingInDefaultLocale[$oid][$field] = true;
+                        $originalValue = null;
+                    } else if (empty($translated)) {
+                        $translated = $this->getFallbackTranslation($originalValue);
+                    } else {
+                        $originalValue = null;
+                    }
                 }
 
                 // update translation
@@ -839,6 +852,10 @@ class TranslatableListener extends MappedEventSubscriber
                         if (null !== $this->getTranslationInDefaultLocale($oid, $field)) {
                             $wrapped->setPropertyValue($field, $this->getTranslationInDefaultLocale($oid, $field)->getContent());
                             $this->removeTranslationInDefaultLocale($oid, $field);
+                        } else if (!empty($this->missingInDefaultLocale[$oid][$field])) {
+                            $defaultValue = $this->getFallbackUntranslation($changeSet[$field][1]);
+                            $wrapped->setPropertyValue($field, $defaultValue);
+                            unset($this->missingInDefaultLocale[$oid][$field]);
                         }
                     }
                     $ea->recomputeSingleObjectChangeset($uow, $meta, $object);
