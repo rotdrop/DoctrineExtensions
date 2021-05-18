a6361ecd8695b0a7ac6b111e9d5cfb1d18fef26a
Claus-Justus Heine <himself@claus-justus-heine.de>
2021-05-18 22:48:46 +0200

    Allow composite identifiers in translatable
    See doctrine-extensions/DoctrineExtensions#2171
    
    In the same spirit allow composite identifiers for translatable.
    
    The tree-walker stuff still will not work for composite identifiers.

--- src/Translatable/Mapping/Driver/Annotation.php
+++ src/Translatable/Mapping/Driver/Annotation.php
@@ -118,8 +118,8 @@ class Annotation extends AbstractAnnotationDriver
         }
 
         if (!$meta->isMappedSuperclass && $config) {
-            if (is_array($meta->getIdentifier()) && count($meta->getIdentifier()) > 1) {
-                throw new InvalidMappingException("Translatable does not support composite identifiers in class - {$meta->getName()}");
+            if ($meta instanceof \Doctrine\ODM\MongoDB\Mapping\ClassMetadata && is_array($meta->identifier) && count($meta->identifier) > 1) {
+                throw new InvalidMappingException("Translatable does not support composite identifiers in class - {$meta->name}");
             }
         }
 
