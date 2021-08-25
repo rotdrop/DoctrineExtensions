66f0c510f8f797a78e77a2d68cb0d573413d67a7
Claus-Justus Heine <himself@claus-justus-heine.de>
2021-08-25 15:30:19 +0200

    Fix PHP 8 warning -> error issue

--- src/SoftDeleteable/SoftDeleteableListener.php
+++ src/SoftDeleteable/SoftDeleteableListener.php
@@ -134,7 +134,7 @@ class SoftDeleteableListener extends MappedEventSubscriber
             return;
         }
 
-        foreach ($config['cascadeDelete'] as $cascadeField) {
+        foreach (($config['cascadeDelete']??[]) as $cascadeField) {
             $association = $meta->getReflectionProperty($cascadeField)->getValue($object);
             if ($meta->isCollectionValuedAssociation($cascadeField)) {
                 $collection = $association;
