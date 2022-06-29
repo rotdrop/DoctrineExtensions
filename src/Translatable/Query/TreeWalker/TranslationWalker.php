f73eda28aeb7dae7958ce91b6b7555e16974ea92
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-06-29 08:40:50 +0200

    Fix typo in comment

--- src/Translatable/Query/TreeWalker/TranslationWalker.php
+++ src/Translatable/Query/TreeWalker/TranslationWalker.php
@@ -329,7 +329,7 @@ class TranslationWalker extends SqlOutputWalker
                         array_keys($quotedIds));
 
                     // this works as long as the identifier flattening code in
-                    // src/Tool/Wrapper uses the same ordering or identifier
+                    // src/Tool/Wrapper uses the same ordering as identifier
                     // columns ...
                     $fkConcat = 'CONCAT(' . implode(", ' ',", $idColNames) . ')';
 
