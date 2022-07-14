02fb1df7178e98c4026ee759b7112f9a30ede21c
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-07-14 15:16:23 +0200

    setTranslationValue(): return the computed field-value s.t. it can be reused by the caller.

--- src/Translatable/Mapping/Event/Adapter/ORM.php
+++ src/Translatable/Mapping/Event/Adapter/ORM.php
@@ -222,6 +222,8 @@ final class ORM extends BaseAdapterORM implements TranslatableAdapter
         $type = Type::getType($meta->getTypeOfField($field));
         $value = $type->convertToPHPValue($value, $em->getConnection()->getDatabasePlatform());
         $wrapped->setPropertyValue($field, $value);
+
+        return $value;
     }
 
     /**
