d53691718aebfb8f4d7282d5a1a02618f89e60b6
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-09-17 02:04:04 +0200

    Revert "AbstractTrackingListener::updateField() -- omit unmanaged objects (happens after deletion)"
    This reverts commit b5e584eaf2a6f1eb92f438b676b61c0855a83e1b.

--- src/AbstractTrackingListener.php
+++ src/AbstractTrackingListener.php
@@ -328,12 +328,11 @@ abstract class AbstractTrackingListener extends MappedEventSubscriber
 
         $changed = [];
         foreach ($collection as $targetObject) {
-            if ($uow->getEntityState($targetObject) != UnitOfWork::STATE_MANAGED) {
-                continue;
-            }
             $wrappedTarget = AbstractWrapper::wrap($targetObject, $om);
-            $oldValue = $wrappedTarget->getPropertyValue($targetField);
-            $eventAdapter->setOriginalObjectProperty($uow, $targetObject, $targetField, $oldValue);
+            if ($uow->getEntityState($targetObject) == UnitOfWork::STATE_MANAGED) {
+                $oldValue = $wrappedTarget->getPropertyValue($targetField);
+                $eventAdapter->setOriginalObjectProperty($uow, $targetObject, $targetField, $oldValue);
+            }
             $wrappedTarget->setPropertyValue($targetField, $newValue);
             if ($targetObject instanceof NotifyPropertyChanged) {
                 if (empty($oldValue)) {
