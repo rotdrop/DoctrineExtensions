d417c6a2c365a50a0e5718d11adf6e0956d7737c
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-10-18 16:49:10 +0200

    Locale annotation was missing the @NamedArgumentConstructor annotation.

--- src/Mapping/Annotation/Locale.php
+++ src/Mapping/Annotation/Locale.php
@@ -17,6 +17,7 @@ use Gedmo\Mapping\Annotation\Annotation as GedmoAnnotation;
  * Locale annotation for Translatable behavioral extension
  *
  * @Annotation
+ * @NamedArgumentConstructor
  * @Target("PROPERTY")
  *
  * @author Gediminas Morkevicius <gediminas.morkevicius@gmail.com>
